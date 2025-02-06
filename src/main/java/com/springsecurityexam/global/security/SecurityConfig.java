9d26973921dbc512be1f7819ccf3cda635896d90
socra167 <socra167@gmail.com>
2025-02-06 15:33:31 +0900

    chore: spring security h2-console 접속 설정, csrf 해제

--- src/main/java/com/springsecurityexam/global/security/SecurityConfig.java
+++ src/main/java/com/springsecurityexam/global/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,12 +17,18 @@ public class SecurityConfig {
 		http
 			.authorizeHttpRequests((authorizeHttpRequests) ->
 				authorizeHttpRequests
+					.requestMatchers("/h2-console/**")
+					.permitAll()
 					.requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts",
 						"/api/*/posts/{postId:\\d+}/comments")
 					.permitAll()
 					.anyRequest()
 					.authenticated()
-			);
+			)
+			.headers((headers) -> headers
+				.addHeaderWriter(new XFrameOptionsHeaderWriter(
+					XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+			.csrf((csrf) -> csrf.disable());
 		return http.build();
 	}
 }
